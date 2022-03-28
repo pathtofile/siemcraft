4d51be647dd60c08e1b440e24d497552037dd862
pathtofile <path@tofile.dev>
2022-03-28 16:39:12 +1100

    start clean

--- run.ps1
+++ run.ps1
@@ -3,6 +3,4 @@ $ErrorActionPreference = "Stop"
 . "$PSScriptRoot\build.ps1"
 
 # Run
-# .\bin\siemcraft.exe -noKill
-# .\bin\siemcraft.exe -fakeEvents -rules C:\code\siemcraft\sigma\rules
-.\bin\siemcraft.exe -channels "Sealighter/Operational"
+"$PSScriptRoot\bin\siemcraft.exe -rules $PSScriptRoot\rules"
