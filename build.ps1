acdc3fe277a845146632443676b4bbc6614cf998
pathtofile <path@tofile.dev>
2022-06-11 09:23:35 +1000

    fixed

--- build.ps1
+++ build.ps1
@@ -2,8 +2,9 @@
 $ErrorActionPreference = "Stop"
 New-Item -ItemType Directory -Force -Path "$PSScriptRoot\bin"
 Compress-Archive -Path "$PSScriptRoot\siemcraft_addon_behavior" -DestinationPath "$PSScriptRoot\bin\siemcraft_addon_behavior.mcpack" -Force
+Compress-Archive -Path "$PSScriptRoot\siemcraft_addon_gametest" -DestinationPath "$PSScriptRoot\bin\siemcraft_addon_gametest.mcpack" -Force
 Compress-Archive -Path "$PSScriptRoot\siemcraft_addon_resource" -DestinationPath "$PSScriptRoot\bin\siemcraft_addon_resource.mcpack" -Force
-Compress-Archive -Path "$PSScriptRoot\bin\*.mcpack -DestinationPath" "$PSScriptRoot\bin\siemcraft.mcaddon" -Force
+Compress-Archive -Path "$PSScriptRoot\bin\*.mcpack" -DestinationPath "$PSScriptRoot\bin\siemcraft.mcaddon" -Force
 
 # Build Server
 go build -o "$PSScriptRoot\bin\siemcraft.exe" ./src
